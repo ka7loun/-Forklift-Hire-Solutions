1e05aa1ac16ad371d8d391d0e1880ac1f6cca460
ka7loun <kahlounahmed1.com>
2025-04-03 18:48:04 +0100

    Fix base URL to match exact repository name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/forklift-Hire-Solutions/',
+  base: '/-Forklift-Hire-Solutions/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
